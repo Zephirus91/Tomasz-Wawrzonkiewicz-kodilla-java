c2a2f6815aa0f5cde0bdb46fe45e182c87447486
Tomasz Wawrzonkiewicz <tomaszwaw1991@gmail.com>
2021-01-21 20:20:14 +0100

    13.2 Order Service

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MoneyTransfer.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MoneyTransfer.java
@@ -2,6 +2,6 @@ package com.kodilla.good.patterns.challenges;
 
 public class MoneyTransfer implements PaymentService {
     public void newPayment(Client client, Seller seller) {
-        System.out.println("Client " + client + " has transfered money to " + seller + ". ");
+        System.out.println("Client " + client + " has transfered money to " + seller + ".");
     }
 }
