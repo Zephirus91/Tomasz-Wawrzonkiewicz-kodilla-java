b9135f579caa0deef2968e2a77ff2e7eabbeb25b
Tomasz Wawrzonkiewicz <tomaszwaw1991@gmail.com>
2021-05-16 21:20:27 +0200

    24.5 fully qualified path update

--- kodilla-paterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-paterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -1,7 +1,6 @@
 package com.kodilla.patterns2.adapter.bookclasifier;
 
 import com.kodilla.patterns2.adapter.bookclasifier.librarya.Classifier;
-import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
 import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
 
 import java.util.HashMap;
@@ -11,9 +10,9 @@ import java.util.Set;
 public class MedianAdapter extends MedianAdaptee implements Classifier {
 
     @Override
-    public int publicationYearMedian(Set<Book> bookSet) {
+    public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> bookSet) {
         Map<BookSignature, com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book> bookMap = new HashMap<>();
-        for (Book book : bookSet) {
+        for (com.kodilla.patterns2.adapter.bookclasifier.librarya.Book book : bookSet) {
             bookMap.put(new BookSignature(book.getSignature()),
                     new com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book(book.getAuthor(), book.getTitle(), book.getPublicationYear()));
         }
